ff7ce51bea3d8869966b4a90a6e59571a9dfcb88
HappyKit <3123557609@qq.com>
2022-09-19 16:04:28 +0800

    first commit

---
+++ src/main/java/com/yao/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.yao.git;
+
+/**
+ * @author HappyKit
+ * @version 1.5
+ * @date 2022/9/19 15:58
+ * @Description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
